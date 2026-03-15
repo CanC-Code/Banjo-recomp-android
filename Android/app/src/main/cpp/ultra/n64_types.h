2a61e782e14abe9f3f0cdec86111d5d07b598607
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-03-14 22:16:00 -0600

    Update n64_types.h

--- Android/app/src/main/cpp/ultra/n64_types.h
+++ Android/app/src/main/cpp/ultra/n64_types.h
@@ -3,7 +3,6 @@
 
 /**
  * 1. THE NUCLEAR BLOCKADE
- * Prevents legacy headers from being loaded later in the chain.
  */
 #define _ULTRA64_H_
 #define __ULTRA64_H__
@@ -30,14 +29,11 @@
 #define _BOOL_H_      
 #define __BOOL_H__
 #define BOOL_H
-#define _N64_BOOL_H_ // Added extra potential guard
 #define OSINT_H
 #define _OSINT_H_INCLUDED
 
 /**
- * 2. ALL N64 TYPES (PRIMITIVES & OS)
- * These MUST be defined before any #include to resolve the errors seen in 
- * structs.h and sched.h.
+ * 2. CORE N64 TYPES
  */
 typedef signed char            s8;
 typedef unsigned char          u8;
@@ -52,8 +48,8 @@ typedef double                 f64;
 typedef unsigned char          uchar;
 typedef volatile unsigned int  vu32; 
 
-// Primitives for the sanitization script
-typedef u8 n64_bool;
+// Matches the 'int' definition in your project's bool.h to prevent redefinition errors
+typedef int n64_bool;
 #ifndef TRUE
   #define TRUE 1
 #endif
@@ -114,51 +110,62 @@ typedef struct OSThread_s {
 } OSThread;
 
 /**
- * 3. SYSTEM & PROJECT INCLUDES
- * Now that ALL types are defined, it is safe to load headers that reference them.
+ * 3. SYSTEM INCLUDES
+ * We use include_next to bypass the project's 'string.h' and get the real NDK headers.
  */
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
 
-/* Temporarily lift blockade to load the real headers */
 #undef _STRING_H_
 #undef __STRING_H__
 #undef _SCHED_H_
 #undef __SCHED_H__
 
-#include <string.h>
-#include <math.h>
-#include <sched.h> 
+#ifdef __clang__
+  #include_next <string.h>
+  #include_next <math.h>
+  #include_next <sched.h>
+#else
+  #include <string.h>
+  #include <math.h>
+  #include <sched.h>
+#endif
 
-/* Re-establish blockade */
 #define _STRING_H_
 #define __STRING_H__
 #define _SCHED_H_
 #define __SCHED_H__
 
-// Final cleanup for legacy C null pointer issues
 #undef NULL
 #define NULL 0
 
 /**
- * 4. COMPILER MACRO WRAPPERS
+ * 4. N64 NAMESPACED PROTOTYPES
+ * Instead of macros, we provide external declarations for the namespaced functions
+ * created by your sanitization script.
  */
-#ifndef __cplusplus
-  #define memcpy  n64_memcpy
-  #define memmove n64_memmove
-  #define malloc  n64_malloc
-  #define free    n64_free
-  #define realloc n64_realloc 
-  #define calloc  n64_calloc  
-  #define strcat  n64_strcat
-  #define strcpy  n64_strcpy
-  #define strlen  n64_strlen
-  #define sprintf n64_sprintf
-  #define printf  n64_printf
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-  #undef bcopy
-  #define bcopy(src, dst, n) n64_memmove((dst), (src), (n))
+extern void* n64_memcpy(void* dest, const void* src, size_t n);
+extern void* n64_memmove(void* dest, const void* src, size_t n);
+extern void* n64_malloc(size_t size);
+extern void  n64_free(void* ptr);
+extern void* n64_realloc(void* ptr, size_t size);
+extern void* n64_calloc(size_t nmemb, size_t size);
+extern char* n64_strcat(char* dest, const char* src);
+extern char* n64_strcpy(char* dest, const char* src);
+extern size_t n64_strlen(const char* s);
+extern int   n64_sprintf(char* str, const char* format, ...);
+extern int   n64_printf(const char* format, ...);
+
+#undef bcopy
+#define bcopy(src, dst, n) n64_memmove((dst), (src), (n))
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif // N64_TYPES_H
