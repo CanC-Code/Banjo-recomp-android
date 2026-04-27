59e3ae9ffa57a0db131af769c9630b39fa909677
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-04-27 13:02:08 -0600

    Update n64_types.h

--- Android/app/src/main/cpp/ultra/n64_types.h
+++ Android/app/src/main/cpp/ultra/n64_types.h
@@ -32,193 +32,43 @@
 #define ALEventListItem_s    __orig_ALEventListItem_s
 #define ALVoiceState_s       __orig_ALVoiceState_s
 #define ALVoiceState         __orig_ALVoiceState
-#define ALSeqMarker          __orig_ALSeqMarker
-#define ALCSeqMarker         __orig_ALCSeqMarker
-#define ADPCM_STATE          __orig_ADPCM_STATE
-
-/* Intercept macro enums */
-#define AL_SEQP_LOOP_EVT     __orig_AL_SEQP_LOOP_EVT
-#define AL_MIDI_FX_CTRL_0    __orig_AL_MIDI_FX_CTRL_0
-#define AL_MIDI_FX_CTRL_1    __orig_AL_MIDI_FX_CTRL_1
-#define AL_MIDI_FX_CTRL_2    __orig_AL_MIDI_FX_CTRL_2
-#define AL_MIDI_FX_CTRL_3    __orig_AL_MIDI_FX_CTRL_3
 
 /* =============================================
-   INCLUDE SDK HEADERS
+   STANDARD N64 TYPES REPLACEMENT (ADDED FIX)
+   Map modern standard integer types to legacy N64 types.
    ============================================= */
-#include "PR/ultratypes.h"  // Fundamental types (u8, s8, etc.)
-#include "PR/os.h"
-#include "PR/gbi.h"
-#include "PR/gu.h"
-#ifndef PR_ABI_H_INCLUDED
-#define PR_ABI_H_INCLUDED
-#include "PR/libaudio.h"    // Original Acmd definition
-#endif
+#include <stdint.h>
+
+/* Unsigned types */
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+/* Signed types */
+typedef int8_t   s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+typedef int64_t  s64;
+
+/* Volatile Unsigned types */
+typedef volatile uint8_t  vu8;
+typedef volatile uint16_t vu16;
+typedef volatile uint32_t vu32;
+typedef volatile uint64_t vu64;
+
+/* Volatile Signed types */
+typedef volatile int8_t   vs8;
+typedef volatile int16_t  vs16;
+typedef volatile int32_t  vs32;
+typedef volatile int64_t  vs64;
+
+/* Floating point types */
+typedef float  f32;
+typedef double f64;
 
 /* =============================================
-   ACMD COMPATIBILITY
-   ============================================= */
-#ifndef BKA_ACMD_COMPAT
-#define BKA_ACMD_COMPAT
-
-/* Define our Acmd type to match PR/abi.h's union */
-typedef union {
-    u32 w0;
-    u32 w1;
-} BKA_Acmd_Compat;
-
-/* Make Acmd point to our compatible type */
-#undef Acmd
-#define Acmd BKA_Acmd_Compat
-
-/* Redefine aClearBuffer for our Acmd type */
-#undef aClearBuffer
-#define aClearBuffer(_a, _d, _c) \
-    (_a)->w0 = _SHIFTL(A_CLEARBUFF, 24, 8) | _SHIFTL((_d), 0, 24), \
-    (_a)->w1 = (unsigned int)(_c)
-
-#endif /* BKA_ACMD_COMPAT */
-
-/* =============================================
-   RESTORE ORIGINAL NAMESPACE
-   ============================================= */
-#undef ALLink_s
-#undef ALLink
-#undef ALVoice_s
-#undef ALVoice
-#undef N_ALVoice_s
-#undef N_ALVoice
-#undef ALSynth
-#undef ALSyn
-#undef ALGlobals
-#undef ALGlobals_s
-#undef ALEvent
-#undef ALEvent_s
-#undef ALEventListItem
-#undef ALEventListItem_s
-#undef ALVoiceState_s
-#undef ALVoiceState
-#undef ALSeqMarker
-#undef ALCSeqMarker
-#undef ADPCM_STATE
-
-#undef AL_SEQP_LOOP_EVT
-#undef AL_MIDI_FX_CTRL_0
-#undef AL_MIDI_FX_CTRL_1
-#undef AL_MIDI_FX_CTRL_2
-#undef AL_MIDI_FX_CTRL_3
-
-/* =============================================
-   CUSTOM TYPES (non-SDK)
-   ============================================= */
-typedef struct ALLink_s {
-    struct ALLink_s *next;
-    struct ALLink_s *prev;
-} ALLink;
-
-typedef s32 ALMicroTime;
-
-struct N_ALVoice_s;
-
-typedef struct ALPVoice_s {
-    ALLink node;
-    struct N_ALVoice_s *vvoice;
-    s32 offset;
-} ALPVoice;
-
-typedef ALPVoice PVoice;
-
-typedef struct ALEvent_s {
-    s32 type;
-    union {
-        struct {
-            struct N_ALVoice_s *voice;
-            u8 chan;
-        } note;
-        u8 raw[32];
-    } msg;
-} ALEvent;
-typedef ALEvent N_ALEvent;
-
-typedef struct ALEventListItem_s {
-    ALLink node;
-    ALMicroTime delta;
-    ALEvent *evt;
-} ALEventListItem;
-
-typedef struct N_ALVoice_s {
-    struct N_ALVoice_s *next;
-    ALPVoice *pvoice;
-    void *clientPrivate;
-    s16 unityPitch;
-} N_ALVoice;
-
-typedef N_ALVoice ALVoice;
-
-typedef struct ALVoiceState_s {
-    struct ALVoiceState_s *next;
-    ALVoice *voice;
-    f32 pitch;
-    u8 state;
-} ALVoiceState;
-typedef ALVoiceState N_ALVoiceState;
-
-typedef struct ALFilter_s {
-    struct ALFilter_s *source;
-    Acmd *(*handler)(void *, s16 *, s32, s32, void *);
-    s32 (*setParam)(void *, s32, void *);
-    s32 inp;
-    s32 outp;
-    s32 type;
-} ALFilter;
-
-/* =============================================
-   HARMONIZER INJECTIONS
-   ============================================= */
-#ifndef BKA_HARMONIZER_INJECT
-#define BKA_HARMONIZER_INJECT
-
-typedef struct ALParam_s {
-    struct ALParam_s *next;
-    s32 delta;
-    s16 type;
-    s32 samples;
-    f32 pitch;
-    s16 unity;
-    u8 pan;
-    u8 volume;
-    u8 fxMix;
-    void *wave;
-    union { f32 f; s32 i; } data;
-} ALParam;
-
-typedef ALParam ALStartParam;
-typedef ALParam ALStartParamAlt;
-
-typedef Acmd *(*ALCmdHandler)(void *, s16 *, s32, s32, void *);
-typedef s32 (*ALSetParam)(void *, s32, void *);
-
-#endif /* BKA_HARMONIZER_INJECT */
-
-/* =============================================
-   BANJO-SPECIFIC STRUCTS
-   ============================================= */
-#ifndef BKA_ALSYNTH_DEFINED
-#define BKA_ALSYNTH_DEFINED
-typedef struct ALSynth_s { u8 pad[256]; } ALSynth;
-typedef ALSynth ALSyn;
-#endif
-
-#ifndef BKA_ALGLOBALS_DEFINED
-#define BKA_ALGLOBALS_DEFINED
-typedef struct ALGlobals_s {
-    ALSynth drvr;
-    u8 pad[2048];
-} ALGlobals;
-#endif
-
-/* =============================================
-   SANITIZER SUPPORT
+   SANITIZER & STANDARD LIBS
    ============================================= */
 #ifndef BKA_SANITIZER_SUPPORT_DEFINED
 #define BKA_SANITIZER_SUPPORT_DEFINED
@@ -227,6 +77,7 @@ typedef struct ALGlobals_s {
 #include <string.h>
 #include <math.h>
 
+/* Using the newly defined s32 type! */
 typedef s32 n64_bool;
 
 #ifndef n64_malloc
@@ -267,19 +118,13 @@ typedef s32 n64_bool;
 #ifndef BKA_BANJO_LAYER
 #define BKA_BANJO_LAYER
 
-typedef ALEventListItem N_ALEventListItem;
+typedef struct __orig_ALEventListItem N_ALEventListItem;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern Acmd *n_alAdpcmPull(void *, s16 *, s32, Acmd *);
-extern Acmd *n_alResamplePull(void *, s16 *, Acmd *);
-extern Acmd *n_alEnvmixerPull(void *, s32, Acmd *);
-extern Acmd *n_alSavePull(s32, Acmd *);
-extern Acmd *n_alAuxBusPull(void);
-extern Acmd *n_alFxPull(void);
-extern Acmd *n_alMainBusPull(void);
+/* External layer implementations can be defined below */
 
 #ifdef __cplusplus
 }
