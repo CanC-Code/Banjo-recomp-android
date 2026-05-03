70d2769d9600c3d44a73bd8e5e965c585a4df81d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-05-03 15:20:18 -0600

    Update resource_mgr.cpp

--- Android/app/src/main/cpp/emulator/resource_mgr.cpp
+++ Android/app/src/main/cpp/emulator/resource_mgr.cpp
@@ -10,6 +10,7 @@
 
 #define LOG_TAG "ResourceMgr"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,  LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 // --- Manifest Structure (Must match otr_builder.cpp exactly) ---
